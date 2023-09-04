e3555480f955851a537b7bb478f9f6ee04c0ef84
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-09-04 08:48:38 +0100

    Chore: solved difference of squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        _ => (1..=n).fold(0, |acc, x| acc + x).pow(2),
+    }
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        _ => (1..=n).fold(0, |acc, x| acc + x.pow(2)),
+    }
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}

f801966cc19f25ddbd5a58557619ff45bdee6b37
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-09-04 08:20:53 +0100

    chore: commented main function

--- rust/perfect-numbers/src/lib.rs
+++ rust/perfect-numbers/src/lib.rs
@@ -11,7 +11,8 @@ pub fn classify(num: u64) -> Option<Classification> {
     if num == 0 {
         return None;
     } else {
-        
+        // Match the numbers between 1 and num, filter out the ones that are can divide num
+        // and sum them up. Then compare the sum to num and return the appropriate Classification
         match (1..num).filter(|x| num % x == 0).sum::<u64>().cmp(&num) {
             Ordering::Less => Some(Classification::Deficient),
             Ordering::Equal => Some(Classification::Perfect),

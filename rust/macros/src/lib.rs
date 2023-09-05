dfad9e3cc8b9eb7443f03fd6db283a488da564ce
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-09-05 06:37:27 +0100

    chore: finding out solution

---
+++ rust/macros/src/lib.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+#[macro_export]
+macro_rules! hashmap {
+    () => {
+        HashMap::new()
+    };
+    ($($key:expr => $value:expr),+ $(,)?) => {
+        {
+            let mut map = HashMap::new();
+            $(
+                map.insert($key, $value);
+            )+
+            map
+        }
+    };
+}

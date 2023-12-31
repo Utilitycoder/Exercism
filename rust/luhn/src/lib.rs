7e025d41642817106a416fba4314d5d04c166442
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-12-31 15:50:13 +0100

    Chore: solved luhn algorithm problem

---
+++ rust/luhn/src/lib.rs
@@ -0,0 +1,18 @@
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    let mut sum = 0;
+    let mut count = 0;
+    for c in code.chars().rev().filter(|d| !d.is_whitespace()) {
+        if let Some(digit) = c.to_digit(10) {
+            if count % 2 == 1 {
+                sum += digit * 2 - if digit > 4 {9} else {0};
+            } else {
+                sum += digit;
+            }
+            count += 1;
+        } else {
+            return false;
+        }
+    }
+    count > 1 && sum % 10 == 0
+}
